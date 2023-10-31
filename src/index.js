985bc5fb7bae15309fe3a41579997d0edb5636f0
izhoopes <ianzhoopes@gmail.com>
2023-10-30 21:52:55 -0400

    It should build now

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,61 +18,6 @@ root.render(
 );
 
 
-function navigation(){
-	return(
-		<Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-	);
-}
-
-//bad function name, don't care
-function CardBois(){
-	<div>
-	
-	<Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Card 1</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-    </Card>
-	<Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Card 2</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-    </Card>
-	<Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Card 3</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-    </Card>
-	
-	</div>
-};
-
-
-export default CardBois;
-
-export {
-  navigation,
-  CardBois,
-}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/lab06.js
+++ src/lab06.js
@@ -1,4 +1,11 @@
+//import Card from 'react-bootstrap/Card';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import "bootstrap/dist/css/bootstrap.css";
+
+
 
 function navigation(){
 	return(
