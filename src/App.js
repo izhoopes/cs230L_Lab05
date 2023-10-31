1a7b2c62d4a76bba86b96b06e4e01e84334cd4ab
izhoopes <ianzhoopes@gmail.com>
2023-10-30 20:18:46 -0400

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import './index.js';
+import navigation from './index.js';
+import CardBois from './index.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>CS 230L</h1>
+      <navigation></navigation>
+	  <h1>CS 230L</h1>
 	  <h2>Section - 002</h2>
 	  <p>WVU ID: 800368267</p>
 	  <p>Hey, I'm Ian Hoopes</p>
-    </div>
+	  <CardBois></CardBois>
+	</div>
   );
 }
 
