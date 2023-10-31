ebcf477c21d88c76994aa069379c983c8450b3cd
izhoopes <ianzhoopes@gmail.com>
2023-10-30 21:59:35 -0400

    thing

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import CardBois from './lab06.js';
 function App() {
   return (
     <div className="App">
-      <navigation></navigation>
+      <navigation/>
 	  <h1>CS 230L</h1>
 	  <h2>Section - 002</h2>
 	  <p>WVU ID: 800368267</p>
 	  <p>Hey, I'm Ian Hoopes</p>
-	  <CardBois></CardBois>
+	  <CardBois/>
 	</div>
   );
 }
