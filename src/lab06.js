08794bdd3c098f98a28e37076142b9dc6ccaf04a
izhoopes <ianzhoopes@gmail.com>
2023-10-30 20:55:21 -0400

    moved navigation and CardBois out of index.js and into lab06.js

---
+++ src/lab06.js
@@ -0,0 +1,54 @@
+function navigation(){
+	return(
+		<Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+	);
+}
+
+//bad function name, don't care
+function CardBois(){
+	<div>
+	
+	<Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card 1</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+	<Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card 2</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+	<Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card 3</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+	
+	</div>
+};
+
+
+
+export {
+  navigation,
+  CardBois,
+}
